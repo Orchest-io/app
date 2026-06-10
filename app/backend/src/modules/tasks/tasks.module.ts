02bd6d3d85af27da261b4414bf33b8047d8cc152
Bassel Raafat <basselr11@gmail.com>
2026-06-11 00:29:56 +0300

    feat: update tasks module with enhanced functionality

--- app/backend/src/modules/tasks/tasks.module.ts
+++ app/backend/src/modules/tasks/tasks.module.ts
@@ -9,11 +9,15 @@ import { TaskDependency } from './entities/task-dependency.entity';
 import { Comment } from './entities/comment.entity';
 import { Attachment } from './entities/attachment.entity';
 import { ProjectsModule } from '../projects/projects.module';
+import { UsersModule } from '../users/users.module';
+import { AnalyticsModule } from '../analytics/analytics.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Task, Subtask, TaskAssignee, TaskDependency, Comment, Attachment]),
     forwardRef(() => ProjectsModule),
+    UsersModule,
+    AnalyticsModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
